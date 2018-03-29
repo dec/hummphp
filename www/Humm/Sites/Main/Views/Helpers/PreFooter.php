be0a66b7a70a670e2256d703afa7f01f1833ecdf
David Esperalta <info@davidesperalta.com>
2018-03-29 19:23:42 +0200

    Update the footer copyright notice
    Update the footer copyright notice

--- www/Humm/Sites/Main/Views/Helpers/PreFooter.php
+++ www/Humm/Sites/Main/Views/Helpers/PreFooter.php
@@ -2,7 +2,7 @@
 <?php if (!$this) { exit; } ?>
 
  <footer class="footer text-center">
-  <p>&copy; 2017 DecSoft's Humm PHP - <a href="https://www.davidesperalta.com" title="www.davidesperalta.com">www.davidesperalta.com</a></p>
+  <p>&copy; 2018 DecSoft's Humm PHP - <a href="https://www.davidesperalta.com" title="www.davidesperalta.com">www.davidesperalta.com</a></p>
  </footer>
 
 </div> <!-- /container -->
