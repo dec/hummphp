ca9630858c3d1208fae53279d71e5b22fa70514f
David Esperalta <info@davidesperalta.com>
2018-03-31 19:29:26 +0200

    Add some useful CSS code for Boostrap CSS 4
    Add some useful CSS code for Boostrap CSS 4

--- www/Humm/System/Version/Version.php
+++ www/Humm/System/Version/Version.php
@@ -14,9 +14,9 @@
 /**
  * Define the version of Humm PHP.
  */
-\define('HUMM_VERSION_STRING', '2018.7 (03/29/2018)');
+\define('HUMM_VERSION_STRING', '2018.8 (03/31/2018)');
 
 /**
  * Define the release date of Humm PHP.
  */
-\define('HUMM_VERSION_RELEASE', '03/29/2018');
+\define('HUMM_VERSION_RELEASE', '03/31/2018');
