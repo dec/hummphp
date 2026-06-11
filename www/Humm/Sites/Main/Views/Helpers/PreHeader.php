42b9fad638c5ee3f59115adb57201481c7f28259
David Esperalta <davidesperalta@gmail.com>
2026-06-11 21:48:30 +0200

    Very minor changes
    Very minor changes

--- www/Humm/Sites/Main/Views/Helpers/PreHeader.php
+++ www/Humm/Sites/Main/Views/Helpers/PreHeader.php
@@ -5,7 +5,7 @@
 <html lang="<?= $siteLanguage ?>" dir="<?= $siteLanguageDir ?>">
  <head>
   <meta charset="utf-8">
-  <meta name="generator" content="Humm PHP: https://www.decsoftutils.com/">
+  <meta name="generator" content="Humm PHP: https://github.com/dec">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="<?= $viewsStylesUrl ?>Thirdparty.min.css" media="all">
-  <link rel="stylesheet" type="text/css" href="<?= $viewsStylesUrl ?>Shared.css" media="all">
+  <link rel="stylesheet" href="<?= $viewsStylesUrl ?>Thirdparty.min.css">
+  <link rel="stylesheet" href="<?= $viewsStylesUrl ?>Shared.css">
--- www/Humm/System/Views/Helpers/SystemPreHeader.php
+++ www/Humm/System/Views/Helpers/SystemPreHeader.php
@@ -6,4 +6,4 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="generator" content="Humm PHP: https://www.decsoftutils.com/">
-  <link rel="stylesheet" type="text/css" href="<?= $systemViewsStylesUrl ?>Thirdparty.min.css" media="all">
+  <link rel="stylesheet" href="<?= $systemViewsStylesUrl ?>Thirdparty.min.css">
